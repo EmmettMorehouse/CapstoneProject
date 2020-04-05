9adc9beb5026b7e25e0d3a2b36d8f16991af7302
Emmett Morehouse <emorehou@kent.edu>
2020-04-05 19:25:37 -0400

    Initial arduino code

---
+++ static/Arduino/connections.h
@@ -0,0 +1,9 @@
+/*
+ * Emmett Morehouse/John Yun/Jessica Warren/Brayton Rider
+ * 03/28/2020
+ * Kent State University Capstone Project
+ */
+
+ char mySSID[] = "MySpectrumWiFi20-2G";
+ char myPASS[] = "domainmarket512";
+ char mqttServer[] = "127.0.0.1";
---
+++ static/Arduino/main.ino
@@ -0,0 +1,117 @@
+/*
+ * Emmett Morehouse/John Yun/Jessica Warren/Brayton Rider
+ * 03/28/2020
+ * Kent State University Capstone Project
+ */
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <PubSubClient.h>
+#include "connections.h"
+
+//////Variables////////////////////////////
+
+// WiFi Connection Variables
+const char* ssid = mySSID;        
+const char* pass = myPASS;    
+int status = WL_IDLE_STATUS;
+
+// MQTT Connection Variables
+const char* myServer = mqttServer;
+const char* subTopic = "arduino/sound/sensor";
+const char* pubTopic = "arduino/sound/sensor";
+
+// Sensor Variables
+int sensorPin = A0;
+int sensorValue = 0;
+int decibalValue = 0;
+String soundString;
+
+const char payLoad = (const char)sensorValue;
+///////////////////////////////////////////
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+//Connect to WiFi
+void setup_wifi() {
+  delay(10);
+  
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to network");
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(10000);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) 
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+//Resubscribe to MQTT
+void reconnect() 
+{
+  // Loop until we're reconnected
+  while (!client.connected()) 
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ArduinoClient-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) 
+    {
+      Serial.println("connected");
+      // ... and resubscribe
+      client.subscribe(subTopic);
+    } else 
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(10000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(myServer, 1833);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) 
+  {
+    reconnect();
+  }
+  client.loop();
+  
+  sensorValue = analogRead(sensorPin);
+  decibalValue = (sensorValue+83.2037) / 11.003; // convert analog to decibal
+  Serial.println(decibalValue, DEC); // print for testing
+
+  char payLoad[1];
+  itoa(sensorValue, payLoad, 10);
+  Serial.println(payLoad); // print for testing
+  char toSend[2];
+  itoa(sensorValue, toSend, 10);
+  client.publish(pubTopic, toSend);
+  delay(1000); // read once per second
+}
